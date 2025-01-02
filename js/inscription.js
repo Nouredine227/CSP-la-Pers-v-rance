fad7c21706c43e7c1574e89ab38f6433959d1f10
Nouredine227 <abdoumayakinouredine@gmail.com>
2025-01-02 21:11:05 +0100

    Update inscription.js

--- js/inscription.js
+++ js/inscription.js
@@ -37,7 +37,7 @@ async function submitForm(event) {
 
     try {
         // Récupération du contenu actuel du fichier
-        const response = await fetch(`https://api.github.com/repos/nouredine227/cloud/contents/school/candidats.json}`, {
+        const response = await fetch(`https://api.github.com/repos/nouredine227/cloud/contents/school/candidats.json`, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
@@ -54,7 +54,7 @@ async function submitForm(event) {
         content.push(newCandidate);
 
         // Mise à jour du fichier JSON sur GitHub
-        const updateResponse = await fetch(`https://api.github.com/repos/nouredine227/cloud/contents/school/candidats.json}`, {
+        const updateResponse = await fetch(`https://api.github.com/repos/nouredine227/cloud/contents/school/candidats.json`, {
             method: "PUT",
             headers: {
                 Authorization: `Bearer ${token}`,
